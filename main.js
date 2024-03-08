b37e3bcef18a3362f20b058aca14cf1e833b6316
ZainabMehboob <159926945+ZainabMehboob@users.noreply.github.com>
2024-03-08 12:49:38 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+/*var lenght = 22
+var width = 15
+var areaofrectangle = lenght*width
+
+messageRectangle = `
+Calculating area of Rectangle 
+lenght = 22
+width = 15
+areaofrectangle = ${areaofrectangle}
+`
+
+console.log(messageRectangle);*/
+
+
+
+
+function area(length, width) {
+    
+var result = parseInt(length) * parseInt(width);}
